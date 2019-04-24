b88965fb9689a84343cd8216c519cdd4e039d758
Dillon Larson <z1827320@students.niu.edu>
2019-04-23 22:46:44 -0500

    Homework

---
+++ Computer Science Student/Computer Science Student/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Computer_Science_Student
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void getHoursButton_Click(object sender, EventArgs e)
+        {
+            //Variable to hold input
+            string name, id, track;
+
+            //get the students name and id
+            name = nameTextBox.Text;
+            id = idTextBox.Text;
+
+            //get the students academic track
+            if (infoSystemsRadioButton.Checked)
+            {
+                track = "Information Systems";
+            }
+            else
+            {
+                track = "Software Engineering";
+            }
+
+            //create a CompSciStudent object
+            CompSciStudent csStudent =
+                new CompSciStudent(name, id, track);
+
+            //display the students required hours
+            requiredHoursLabel.Text =
+                csStudent.RequiredHours.ToString("n1");
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Computer Science Student/Computer Science Student/Students.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Computer_Science_Student
+{
+     abstract class Students
+    {
+        //fields
+        private string _name;
+        private string _id;
+
+        //constructor
+        public Students(string name, string id)
+        {
+            _name = name;
+            _id = id;
+        }
+
+        //name property
+        public string name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        //id property
+        public string ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        //required hours property (abstract)
+        public abstract double RequiredHours
+        {
+            get;
+        }
+    }
+}
